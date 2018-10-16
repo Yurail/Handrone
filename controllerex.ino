48deb3f4b131148212cb99a3b4ecc027d1f0d46d
crisphy <quddnr7201@naver.com>
2018-10-16 19:56:05 +0900

    Add files via upload

--- controllerex.ino
+++ controllerex.ino
@@ -6,77 +6,74 @@
 #include <HMC5883L.h>
 #include <MS561101BA.h>
 
-// ------------------------------Defining Constants---------------------------
+// -------------Defining Magnetometer Memory Addresses & Constants------------
 #define HMC5883L_DEFAULT_ADDRESS    0x1E
 #define HMC5883L_RA_DATAX_H         0x03
 #define HMC5883L_RA_DATAZ_H         0x05
 #define HMC5883L_RA_DATAY_H         0x07
-
+int16_t mx, my; // Magnetic direction X & Y
+float heading; // Heading direction
 // ----------------------------Pin Number of Buttons--------------------------
 #define bt1Pin     0x06 //UP
 #define bt2Pin     0x07 //DOWN
 #define bt3Pin     0x08 //RESET ANGLE
 #define bt4Pin     0x09 //EMERGENCY STOP
-
-#define MOVAVG_SIZE         32
-#define STANDARD_PRESSURE   1013.25
-//-----------------------------------------------------------------------------
-
-int16_t mx, my;
-float heading;
-
-MS561101BA baro = MS561101BA();
-float movavg_buff[MOVAVG_SIZE];
-int movavg_i = 0;
-const float sea_press = 1015;
-float pressure, temp;
-int altit;
-
-//value of throttle (0 ~ 1000) controlled by variable resistor
-int throttleVal;
-int Kpcontrol;
-float Kp;
-
+// Initial button state
 int bt1State = HIGH;
 int bt2State = HIGH;
 int bt3State = HIGH;
 int bt4State = HIGH;
+// ------------------------------Defining Barometer Constants-----------------
+#define MOVAVG_SIZE         32
+#define STANDARD_PRESSURE   1013.25
+MS561101BA baro = MS561101BA();
+float movavg_buff[MOVAVG_SIZE];
+int movavg_i = 0;
+const float sea_press = 1015; // Standard sea pressure
+float pressure, temp;
+int altit; // Altitude
+//value of throttle (0 ~ 1000) controlled by variable resistor
+int throttleVal;
+// To control Kp value
+int Kpcontrol;
+float Kp;
+// Offset between Current & Zero angle
 float OFFSET[3] = {0,0,0};
+// Instruction to Drone {Throttle, YAW, ROLL, PITCH}
 int instruction[4] = {0,0,0,0};
-
+// Bluetooth HC-06 port number(4-TX, 5-RX in HC-06)
 SoftwareSerial mySerial(4, 5);
-
+// ------------------------------Defining IMU MPU6050 Constants---------------
 MPU6050 mpu;
 bool dmpReady = false;
 uint16_t packetSize;
 uint16_t fifoCount;
 uint8_t fifoBuffer[64];
-
 Quaternion q;
 VectorFloat gravity;
-float ypr[3];
-int angle[3];
-int anglelimit = 5;
+float ypr[3]; // YAW,ROLL,PITCH
+int angle[3]; // Offseted angles
+int anglelimit = 5; // Threshold
 
 void setup() {
-    Wire.begin();
+    Wire.begin(); // Start wire communications
     //TWBR = 24;
-    Serial.begin(9600);
+    Serial.begin(9600); // Set wire communications speed
     delay(1);
-    mySerial.begin(9600);
+    mySerial.begin(9600); // Set bluetooth communications speed
     delay(1);
-    while (Serial.available() && Serial.read());
+    while (Serial.available() && Serial.read()); // Check wire communications on
     
-    AccelometerSetting();
+    AccelerometerSetting(); // Initialize Accelerometer
     delay(1);
-    MagnetometerSetting();
+    MagnetometerSetting(); // Initialize Magnetometer
     delay(1);
-    //BarometerSetting();
+    //BarometerSetting(); // Initialize Barometer
     //delay(1);
     pinMode(bt1Pin,INPUT_PULLUP);
     pinMode(bt2Pin,INPUT_PULLUP);
     pinMode(bt3Pin,INPUT_PULLUP);
-    pinMode(bt4Pin,INPUT_PULLUP);
+    pinMode(bt4Pin,INPUT_PULLUP); // Make button pins as pullup resistors
 
     delay(2000);
 
@@ -85,15 +82,15 @@ void setup() {
 void loop() {
     //BarometerCalculation();
     
-    MagnetometerCalculation();
+    MagnetometerCalculation(); // Calculate heading direction(YAW)
     
-    AccelometerCalculation();
+    AccelerometerCalculation(); // Calculate angles(ROLL,PITCH)
 
-    ResetButtonControl();
+    ResetButtonControl(); // Make Current angles as Zero angles
     
-    ThrottleButtonControl();    
+    ThrottleButtonControl(); // Change Throttle values
 
-    AngleLimitation();
+    AngleLimitation(); // Set Threshold to angles
     
     /*
     Serial.print(temp);
@@ -120,18 +117,19 @@ void loop() {
     delay(30);
 }
 
-void AccelometerSetting() {
+void AccelerometerSetting() {
     mpu.initialize();
-    mpu.dmpInitialize();
+    mpu.dmpInitialize(); // Initialize
     mpu.setXGyroOffset(220);
     mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788);
+    mpu.setZGyroOffset(-85); // Set Gyroscope Offset
+    mpu.setZAccelOffset(1788); // Set Accelerometer Offset
     mpu.setDMPEnabled(true);
     packetSize = mpu.dmpGetFIFOPacketSize();
-    dmpReady = true;
+    dmpReady = true; // Make DMP mode ready
 }
 void MagnetometerSetting() {
+    // Let Magnetometer Calculated in Accelerometer Chip (MPU6050)
     mpu.setI2CMasterModeEnabled(0);
     mpu.setI2CBypassEnabled(1);
     Wire.beginTransmission(HMC5883L_DEFAULT_ADDRESS);
@@ -141,11 +139,11 @@ void MagnetometerSetting() {
     delay(5);
     Wire.beginTransmission(HMC5883L_DEFAULT_ADDRESS);
     Wire.write(0x00);
-    Wire.write(B00001000);  // 75Hz
+    Wire.write(B00001000);  // 75Hz Speed
     Wire.endTransmission();
     delay(5);
     mpu.setI2CBypassEnabled(0);
-    //x
+    // Setting magnetometer x direction
     mpu.setSlaveAddress(0, HMC5883L_DEFAULT_ADDRESS | 0x80);
     mpu.setSlaveRegister(0, HMC5883L_RA_DATAX_H);
     mpu.setSlaveEnabled(0, true);
@@ -153,7 +151,7 @@ void MagnetometerSetting() {
     mpu.setSlaveWriteMode(0, false);
     mpu.setSlaveWordGroupOffset(0, false);
     mpu.setSlaveDataLength(0, 2);
-    //y
+    // Setting magnetometer y direction
     mpu.setSlaveAddress(1, HMC5883L_DEFAULT_ADDRESS | 0x80);
     mpu.setSlaveRegister(1, HMC5883L_RA_DATAY_H);
     mpu.setSlaveEnabled(1, true);
@@ -161,7 +159,7 @@ void MagnetometerSetting() {
     mpu.setSlaveWriteMode(1, false);
     mpu.setSlaveWordGroupOffset(1, false);
     mpu.setSlaveDataLength(1, 2);
-    //z
+    // Setting magnetometer z direction
     mpu.setSlaveAddress(2, HMC5883L_DEFAULT_ADDRESS | 0x80);
     mpu.setSlaveRegister(2, HMC5883L_RA_DATAZ_H);
     mpu.setSlaveEnabled(2, true);
@@ -212,31 +210,37 @@ float getAvg(float * buff, int size) {
 }
 */
 void MagnetometerCalculation() {
+    // Get data from External Sensor to IMU (MPU6050)
     mx=mpu.getExternalSensorWord(0);
     my=mpu.getExternalSensorWord(2);
+    // Calculate Heading direction
     heading = atan2(my, mx);
 }
 
-void AccelometerCalculation() {
+void AccelerometerCalculation() {
     if(!dmpReady) {
-      ArduinoReset();
+      ArduinoReset(); // If Error in Accelerometer, Reset Whole Arduino
     }
     fifoCount = mpu.getFIFOCount();
     if (fifoCount == 1024) {
-        mpu.resetFIFO();
+        mpu.resetFIFO(); // If Packet Buffer full, Reset
     }
     while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    mpu.getFIFOBytes(fifoBuffer, packetSize); // Get Packet Buffer
     fifoCount -= packetSize;
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    ypr[0] = heading;
+    mpu.dmpGetQuaternion(&q, fifoBuffer); // Get Quaternion from Buffer
+    mpu.dmpGetGravity(&gravity, &q); // Get Gravity from Quaternion
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity); // Get angles from Quaternion & Gravity
+    ypr[0] = heading; // Set Yaw angle from Magnetometer
 }
 
 void ResetButtonControl() {
-    // reset angle with current values
-    if((bt4State!=digitalRead(bt3Pin))&&(digitalRead(bt3Pin)==LOW)) {
+    // reset angles to current values
+    // Current angles becomes Zero angles
+    if((bt3State!=digitalRead(bt3Pin))&&(digitalRead(bt3Pin)==LOW)) {
+      // When Falling Edge, Reset angles
+      // bt3State is LOW in normal, become HIGH when click button
+      // At that moment, bt3State still HIGH, however pin vale becomes LOW
       OFFSET[0] = ypr[0]*180/M_PI;
       OFFSET[1] = ypr[1]*180/M_PI;
       OFFSET[2] = ypr[2]*180/M_PI;
@@ -244,17 +248,17 @@ void ResetButtonControl() {
 }
 
 void ThrottleButtonControl() {
-  //throttleVal = map(analogRead(A0),0,1023,0,1000);
-  if(Serial.available())
-  throttleVal = Serial.parseInt();
-  //Kpcontrol = map(analogRead(A1),0,1023,0,1000);
-  //Kp = float(Kpcontrol)/100;
+  //throttleVal = map(analogRead(A0),0,1023,0,1000); // Change throttle using potentiometer
+  if(Serial.available()) // When Serial Communications available
+  throttleVal = Serial.parseInt(); // Change throttle using Serial Monitor's input
+  //Kpcontrol = map(analogRead(A1),0,1023,0,1000); // Change Kp using potentiometer 
+  //Kp = float(Kpcontrol)/100; // Use value between 0~10
 }
 
 /*
 void ThrottleButtonControl() {
     // Up / Down / Stop values
-    if((bt3State!=digitalRead(bt4Pin))&&(digitalRead(bt4Pin)==LOW)) {
+    if((bt4State!=digitalRead(bt4Pin))&&(digitalRead(bt4Pin)==LOW)) {
       mySerial.print('e');
       Serial.print('e');
     }
@@ -273,20 +277,22 @@ void ThrottleButtonControl() {
 }
 */
 void AngleLimitation(){
+  // Get angle data with Offset values
   angle[0] = int(ypr[0]*180/M_PI-OFFSET[0]);
   angle[1] = int(ypr[1]*180/M_PI-OFFSET[1]);
   angle[2] = int(ypr[2]*180/M_PI-OFFSET[2]);
-  
-  for (int i=0;i<3;i++){
-    if(angle[i]>anglelimit)
-      angle[i] = -2;
-    else if(abs(angle[i])>anglelimit)
-      angle[i] = 2;
+
+  for (int i=0;i<3;i++){ // Repeat 3 times for 3 axis
+    if(angle[i]>anglelimit) // Treat angle above anglelimit(5 degree)
+      angle[i] = -2; // Set values -2
+    else if(abs(angle[i])>anglelimit) //  Treat angle below -anglelimit
+      angle[i] = 2; // Set values 2
     else
-      angle[i] = 0;
+      angle[i] = 0; // Set zero between -5~5 degree for 3 angles
   }
 }
 
+// Arduino Reset function
 void ArduinoReset(){
     asm volatile ("jmp 0");
 }
